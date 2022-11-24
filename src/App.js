39796e64840fb442f5cabafefef5b67103b89c24
sailaja <sailaja.kodehode@gmail.com>
2022-11-24 21:51:19 +0100

    simple styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from "./logo.svg";
-// import "./App.css";
+import "./App.css";
 import UseEffect from "./Components/Task/UseEffect";
 import Context from "./Context/Pokom";
 import Selector from "./Components/Task/Selector";
